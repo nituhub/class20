ea3426e892087cd6dff8b9a62a1d54a31edfff47
nituhub <73851007+nituhub@users.noreply.github.com>
2020-11-28 14:51:58 +0100

    Add files via upload

---
+++ sketch_trial.js
@@ -0,0 +1,31 @@
+var fixedRect, movingRect;
+
+function setup() {
+  createCanvas(1200,1200);
+  fixedRect = createSprite(400, 200, 50, 50);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+
+  movingRect = createSprite(400, 400, 50, 50 );
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+}
+
+function draw() {
+  background(255,255,255); 
+  
+  movingRect.x = World.mouseX; 
+  movingRect.y = World.mouseY;
+
+  if(movingRect.x - fixedRect.x === fixedRect.width/2 + movingRect.width/2  || fixedRect.x - movingRect.x === fixedRect.width/2 + movingRect.width/2){
+    fixedRect.shapeColor = "red";
+    movingRect.shapeColor = "red";
+  }
+  else {
+    fixedRect.shapeColor = "blue";
+    movingRect.shapeColor = "blue";
+  }
+
+
+  drawSprites();
+}
